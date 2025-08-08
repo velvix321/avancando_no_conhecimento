f1b949cc06f327a9965bc13f81b7c7dd07aec206
velvix321 <velvix321@gmail.com>
2025-08-08 11:54:22 -0300

    commit

--- meu_projeto_django/core/settings.py
+++ meu_projeto_django/core/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tarefas', #app novo serve para que o django saiba que tem existe um app
+    'rest_framework',
+    'tarefas',
 ]
 
 MIDDLEWARE = [
--- meu_projeto_django/tarefas/models.py
+++ meu_projeto_django/tarefas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    concluida= models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.titulo
